8e092c600e5cd47bc69323728066b4e0fe47bc50
Matthias Elliott <komizutama@gmail.com>
2019-01-28 19:10:29 -0500

    added ubasic user auth, but there are defineetly bugs, but it's dinner time, so it goes.

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { AuthUserContext, withAuthorization } from '../Session';
 import ChangePasswordForm from "../ChangePassword"
 
 const AccountPage = () => (
-  <div>
-    <h1>Account</h1>
-    <ChangePasswordForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <ChangePasswordForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+// auth user
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(AccountPage);
